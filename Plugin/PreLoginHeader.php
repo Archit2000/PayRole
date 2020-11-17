93069bd37554fe5d249a9f2368256e1887abdf5a
Archit Kane <architpkane@gmail.com>
2020-11-17 17:29:41 +0530

    commit on main

--- Plugin/PreLoginHeader.php
+++ Plugin/PreLoginHeader.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">PayRole</a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+  <a class="navbar-brand" href="index.php">PayRole</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -7,7 +7,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="index.php">Home</a>
       </li>
     </ul>
     <ul class="form-inline my-2 my-lg-0 navbar-nav">
--- index.php
+++ index.php
@@ -1,2 +1,9 @@
 <?php include('Plugin\AddedLibraries.php'); ?>
 <?php include('Plugin\PreLoginHeader.php'); ?>
+
+<div class="row section">
+    <div class="col">asdd</div>
+    <div class="col">dasdd</div>
+</div>
+
+</body></html>
