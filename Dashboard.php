a8354f42f15c9191cd6d0c727983c5553a529382
Archit Kane <architpkane@gmail.com>
2020-11-18 11:54:20 +0530

    commit on main

---
+++ Dashboard.php
@@ -0,0 +1,12 @@
+<?php include('Plugin\AddedLibraries.php'); ?>
+<?php include('Plugin\PostLoginHeader.php'); ?>
+<link rel="stylesheet" href="CSS\dashboard.css">
+
+
+
+<!---ender--->
+</main>
+</div>
+</div>
+</body>
+</html>
--- Plugin/PreLoginHeader.php
+++ Plugin/PreLoginHeader.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="index.php">PayRole</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -11,9 +11,6 @@
       </li>
     </ul>
     <ul class="form-inline my-2 my-lg-0 navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="Sign-Up.php">Sign-Up</a>
-      </li>
       <li class="nav-item active">
         <a class="nav-link" href="Login.php">Login</a>
       </li>
