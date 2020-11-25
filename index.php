8260aca7085182f14282ea5aa1fddbc92e401e35
Archit Kane <architpkane@gmail.com>
2020-11-25 22:44:07 +0530

    commit on main

--- index.php
+++ index.php
@@ -5,7 +5,14 @@
 
 <div class="row section">
     <div class="col">asdd</div>
-    <div class="col">dasdd</div>
+    <div class="col">
+     <div class="row">
+        <h3 class="mx-auto">Payrole Management</h3>
+        <p>
+        
+        </p>
+     </div>
+    </div>
 </div>
 
 </body></html>
